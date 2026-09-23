a864f4781b1ea12c9a336f7277a6f0e6c425e28b
Shridhar <shridhara142004@gmail.com>
2026-09-24 02:44:56 +0530

    Update welcome message

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main()
     printf("Hello Embedded World!\n");
     printf("Learning Git and GitHub!\n");
     printf("Embedded Software Development!\n");
+    printf("My first Git project!\n");
 
     return 0;
 }
